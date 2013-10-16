ab4ec659eafe3a4a6f97d411d6624ac51d5f60ec
Ondrej Chaloupka <ochaloup@redhat.com>
2013-10-16 13:31:54 +0200

    Intial commit

---
+++ src/main/java/org/jboss/qa/TestBean.java
@@ -0,0 +1,22 @@
+package org.jboss.qa;
+
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+
+@Stateless
+public class TestBean {
+	@Resource(lookup="java:jboss/xa-datasources/TestDS")
+	DataSource ds;
+	
+	public void go() {
+		try {
+			ds.getConnection();
+			System.out.println("Go");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/org/jboss/qa/TestBeanTest.java
@@ -0,0 +1,35 @@
+package org.jboss.qa;
+
+import org.jboss.qa.TestBean;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class TestBeanTest
+{
+   @Inject
+   private TestBean testbean;
+   
+   @Deployment
+   public static JavaArchive createDeployment() {
+      return ShrinkWrap.create(JavaArchive.class, "test.jar")
+            .addClass(TestBean.class)
+            .addAsManifestResource("xa-ds.xml")
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+   }
+
+   @Test
+   public void testIsDeployed() {
+      Assert.assertNotNull(testbean);
+      testbean.go();
+   }
+}
