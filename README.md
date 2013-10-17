a7fadb91c5b59e7ede1616706b6a534354970525
Ondrej Chaloupka <ochaloup@redhat.com>
2013-10-17 18:17:49 +0200

    Add cleaning profile, add some info to readme and renaming jndi name of datasource

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Currently it could be maybe used to get a solution to pass properties from maven
 To try it:
 export JBOSS_HOME=<path_to_jboss_eap_home>
 mvn clean test -Djdbc.url=<jdbc-url> -Djdbc.username=<username> -Djdbc.password=<password> -Ddriver=<absolute_path_to_driver_jar>
+or instead of clean to get cleaned the $JBOSS_HOME/standalone/deployments folder as well
+mvn test -Djdbc.url=<jdbc-url> -Djdbc.username=<username> -Djdbc.password=<password> -Ddriver=<absolute_path_to_driver_jar> -Ddelete
+
+when you use the xa-ds.xml you need to specify XA class as well
+mvn test -Djdbc.url=<jdbc-url> -Djdbc.username=<username> -Djdbc.password=<password> -Ddriver=<absolute_path_to_driver_jar> -Djdbc.xa.class
--- src/main/java/org/jboss/qa/TestBean.java
+++ src/main/java/org/jboss/qa/TestBean.java
@@ -1,5 +1,6 @@
 package org.jboss.qa;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.annotation.Resource;
@@ -8,15 +9,26 @@ import javax.sql.DataSource;
 
 @Stateless
 public class TestBean {
-  @Resource(lookup = "java:jboss/xa-datasources/TestDS")
+  @Resource(lookup = "java:jboss/datasources/TestDS")
   DataSource ds;
 
   public void go() {
+    Connection c = null;
+    
     try {
-      ds.getConnection();
       System.out.println("Go");
+      c = ds.getConnection();
+      // c.nativeSQL("CREATE TABLE TEST (id int)");
     } catch (SQLException e) {
       e.printStackTrace();
+      throw new RuntimeException(e);
+    } finally {
+      try {
+        c.close();
+      } catch(SQLException e2) {
+        e2.printStackTrace();
+      }
     }
+    
   }
 }
